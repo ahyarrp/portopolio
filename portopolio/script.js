cb42c2cb996d11ed497df9e5b0790b29530ecb0e
ahyarrp <158565787+ahyarrp@users.noreply.github.com>
2025-05-18 14:24:05 +0700

    Add files via upload

---
+++ portopolio/script.js
@@ -0,0 +1,5 @@
+// Contoh efek interaktif: alert ketika klik nama
+document.querySelector("h1").addEventListener("click", () => {
+    alert("Terima kasih sudah mengunjungi portofolio saya!");
+  });
+  
