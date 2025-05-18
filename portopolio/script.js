c17732ce9d8d3efe685dd458c619c524ee316c17
ahyarrp <158565787+ahyarrp@users.noreply.github.com>
2025-05-18 16:06:49 +0700

    Delete portopolio directory

--- portopolio/script.js
+++
@@ -1,5 +0,0 @@
-// Contoh efek interaktif: alert ketika klik nama
-document.querySelector("h1").addEventListener("click", () => {
-    alert("Terima kasih sudah mengunjungi portofolio saya!");
-  });
-  
